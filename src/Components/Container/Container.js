9d1d884c0ab4a85563241fdb719860a9bb5310db
JoseTbn <josebatumeni@gmail.com>
2021-10-27 20:14:10 +0200

    color titlte

--- src/Components/Container/Container.js
+++ src/Components/Container/Container.js
@@ -1,17 +1,29 @@
 import React from 'react'
 import './container.css'
+import 'antd/dist/antd.css'
+import  { Input  } from 'antd';
+const { Search } = Input;
 
 export default function Container() {
   return (
+    <>
     <div className="container">
 <div className="container--card">
-    <h1>Wheather</h1>
+    <h1 style={{color:'#ffffffd0'}}>Wheather</h1>
     
+    <div>
+    <Search placeholder="input search text" enterButton="Search" size="large" loading />
+   </div>
+ 
 
     
     </div>
 
     </div>
    
-  )
+   
+    </>
+  
+
+ ) 
 }
