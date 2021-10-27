84ffdbc6bf0b69da6036570d8203388e104f22b9
JoseTbn <josebatumeni@gmail.com>
2021-10-27 19:59:14 +0200

    card done - added antd

---
+++ src/Components/Container/Container.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './container.css'
+
+export default function Container() {
+  return (
+    <div className="container">
+<div className="container--card">
+    <h1>Wheather</h1>
+    
+
+    
+    </div>
+
+    </div>
+   
+  )
+}
