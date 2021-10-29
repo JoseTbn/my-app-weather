57fb288eeb9d04e93c9aa4001a3566e98bf76b3e
JoseTbn <josebatumeni@gmail.com>
2021-10-29 11:54:02 +0200

    added redux tools

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import sliceweather from '../slices/wslices'
 
+const devtools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
 const store = configureStore({
     reducer: sliceweather
-});
+},devtools);
 
 export default store 
